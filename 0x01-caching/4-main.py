7c7475360ffadda478c5bd586c348d36e6bccdda
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-26 17:34:14 +0300

    MM

--- 0x01-caching/4-main.py
+++ 0x01-caching/4-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ 4-main """
-MRUCache = __import__('4-mru_cache').MRUCache
+MRUCache = __import__('test').MRUCache
 
 my_cache = MRUCache()
 my_cache.put("A", "Hello")
@@ -13,18 +13,4 @@ my_cache.put("E", "Battery")
 my_cache.print_cache()
 my_cache.put("C", "Street")
 my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-my_cache.put("F", "Mission")
-my_cache.print_cache()
-my_cache.put("G", "San Francisco")
-my_cache.print_cache()
-my_cache.put("H", "H")
-my_cache.print_cache()
-my_cache.put("I", "I")
-my_cache.print_cache()
-my_cache.put("J", "J")
-my_cache.print_cache()
-my_cache.put("K", "K")
-my_cache.print_cache()
+
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,38 +1,33 @@
-#!/usr/bin/env python3
-"""
-BasicCache class
-"""
+#!/usr/bin/python3
+''' MRC Caching '''
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """
-    class FIFOCache that inherits from BaseCaching and is a caching system
-    """
+    ''' A caching system that folow MRU algorithm '''
+    elements_key = []  # Sorted by most used
 
     def put(self, key, item):
-        """ put some items in the dictionary """
-        if key is not None and item is not None:
-            length = len(self.cache_data)
-            if length == BaseCaching.MAX_ITEMS:
-                count = 0
-                for key_1 in self.cache_data:
-                    if key != key_1:
-                        if count == (length - 1):
-                            print(f"DISCARD: {key_1}")
-                            del self.cache_data[key_1]
-                            break
-                    else:
-                        break
-                    count += 1
+        ''' Assigns key to it's item in the chache dict
+            if data is more than the max_limit we pop
+            we discard the Most recently used element '''
+        if key is not None or item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discard_element = ''
+                if key not in list(self.cache_data.keys()):
+                    discard_element = MRUCache.elements_key.pop(0)
+                    del self.cache_data[discard_element]
+                    print("DISCARD:", discard_element)
+                else:
+                    MRUCache.elements_key.remove(key)
+            MRUCache.elements_key.insert(0, key)
             self.cache_data[key] = item
 
     def get(self, key):
-        """ return the value in the dicionary """
-        if key is None or key not in self.cache_data:
-            return None
-        else:
-            value = self.cache_data[key]
-            del self.cache_data[key]
-            self.cache_data[key] = value
-            return value
+        ''' Gets a certain item by it's key and adds them in the
+        end of the list as the were recently used '''
+        value = self.cache_data.get(key, None)
+        if value is not None:
+            MRUCache.elements_key.remove(key)
+            MRUCache.elements_key.insert(0, key)
+        return value
