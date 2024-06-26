44f93540226b4bb6df2b867024526e2826a2aa3d
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-06-26 15:37:14 +0300

    MM

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+BasicCache class
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    class FIFOCache that inherits from BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """ put some items in the dictionary """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                for key in self.cache_data:
+                    print(f"DISCARD: {key}")
+                    del self.cache_data[key]
+                    break
+
+    def get(self, key):
+        """ return the value in the dicionary """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
