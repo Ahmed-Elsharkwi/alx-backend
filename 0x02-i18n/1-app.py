e1ec2d9f1cef524c0ba4666eb31eb185fb287a46
Ahmed-Elsharkwi <ahmedelsharkwiti@gmail.com>
2024-07-02 17:32:49 +0300

    MM

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ module flask """
-from flask import Flask
 from flask_babel import Babel
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -13,6 +14,16 @@ class Config:
 
 app.config.from_object(Config)
 babel.init_app(app)
-
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
